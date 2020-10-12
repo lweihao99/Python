68631701404cc8ca1d124778bf2376b15ded259a
Dr-Zen <weihaoliu99@gmail.com>
2020-10-12 23:52:32 +0200

    2020-10-12

--- Django/example/App/views.py
+++ Django/example/App/views.py
@@ -1,3 +1,4 @@
+from App.models import User
 from django.http import request
 from django.http.response import HttpResponse
 from django.shortcuts import render
@@ -7,7 +8,9 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse('Home page')
-    return render(request, './index.html', context={'title': 'Django example', 'name': 'Weihao'})
+    # 查询数据库
+    users = User.objects.all()
+    return render(request, './index.html', context={'title': 'Django example', 'name': 'Weihao', 'users': users})
 
 # 文件路径得弄对,并且写相对路径
 # request是请求对象
